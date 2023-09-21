22427200b1dbf8d5b0bab60222ae4fe3eb75c933
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2023-09-21 06:42:36 -0500

    Initialize project

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const app = document.getElementById('root');
+
+const root = ReactDOM.createRoot(app);
+
+root.render(
+  <React.Fragment>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+  </React.Fragment>
+);
